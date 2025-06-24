63a01cd3c942c4a9ac6310daf9e517b35115d418
jun-hwan00 <0528klkl@naver.com>
2025-06-24 19:11:35 +0900

    1

--- mysite/mysite/settings.py
+++ mysite/mysite/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = "django-insecure-eyb8$^gxe_ac42sz)uf8%!ez2xmndc%&b)s=!9f-im9tn%z7al
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['junhwan00.pythonanywhere.com']
+ALLOWED_HOSTS = ['junhwan00.pythonanywhere.com','127.0.0.1', '10.0.2.2', '.pythonanywhere.com']
 
 # Application definition
 
